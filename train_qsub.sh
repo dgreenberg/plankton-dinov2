abe137d0f24fc867127641c3bf365c50b83c93fd
NoraKrbr <nora.koreuber@mdc-berlin.de>
2024-01-17 07:50:19 +0100

    update config and train qsub script

--- train_qsub.sh
+++ train_qsub.sh
@@ -1,28 +1,21 @@
-#!/bin/bash
-### chdir in the correct folder and activate the correct python environment. Then run on login node with: qsub ./train.sh <args>
-### some explanation:
-### -V: currently set env variables (e.g. conda) are transferred
-### -cwd: job is started in current directory
-### -l h=maxg03,h=maxg04,h=maxg05,h=maxg06,h=maxg07,h=maxg08,gpu=1: nodes with the V100 GPUs installed (not T4) - will change soon!
-
-
-### $ -l gpu=1 -l cuda_name="Tesla-V100-SXM2-32GB"
-### $ -l gpu=1 -l cuda_name="Tesla-V100-PCIE-32GB"
-### $ -l gpu=1 -l cuda_name="Tesla-V100-SXM2-16GB"
-### $ -l h=maxg03,h=maxg04,h=maxg05,h=maxg06,h=maxg07,h=maxg08,gpu=1
-#$ -l gpu=8
+#$ -l gpu=1
 #$ -l cuda_memory=45G
 #$ -pe smp 4
 #$ -cwd
 #$ -V
-#$ -e error_log_$JOB_ID
-#$ -o out_log_$JOB_ID
-#$ -l h_rt=96:00:00
+#$ -e /home/nkoreub/output_dir/log_$JOB_ID.err
+#$ -o /home/nkoreub/output_dir/log_$JOB_ID.out
+#$ -l h_rt=150:00:00
 #$ -A kainmueller
 
-torchrun "$@"
+N_GPUS=1
+echo $JOB_ID
+torchrun --standalone --nnodes=1 --nproc_per_node=$N_GPUS dinov2/run/train/train.py --ngpus $N_GPUS --config-file dinov2/configs/train/whoi.yaml --run_name=${JOB_ID}_${N_GPUS}gpu_norastestrun train.augmentations=kornia_gpu train.output_dir=/home/nkoreub/output_dir train.use_torch_compile=true train.dataset_path=LMDBDataset:split=ALL:root=/fast/AG_Kainmueller/plankton/data/WHOI/preprocessed_hdf5/lmdb/:extra=*
+
+
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Error"
     exit 100
 fi
+exit 0
